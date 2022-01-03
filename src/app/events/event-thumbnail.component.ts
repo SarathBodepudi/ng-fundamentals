d1fe43a8ade37f7f1f02b92e5a882d8aeac094bd
Sarath Bodepudi <sarathbabubodepudi@gmail.com>
2022-01-02 21:23:46 -0600

    Styling components with ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,12 +8,34 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
       <h2>{{event?.name}}</h2>
       <div></div>
       <div>Date: {{event?.date}}</div>
-      <div [ngSwitch]="event?.time">
+      <!-- Using class. only for one CSS class -->
+      <!--
+      <div [class.green] = "event?.time === '8:00 am'" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
       </div>
+      -->
+
+      <!-- Using ngClass for multiple css class files-->
+      <!--
+      <div [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      -->
+
+      <!-- Simpliyfying the ngclass usage with function-->
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+
       <div>Price: \${{event?.price}}</div>
       <div  *ngIf="event?.location">
         <span>Location :{{event?.location?.address}}</span>
@@ -28,10 +50,31 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     .pad-left {margin-left: 10px}
     .well div {color: #bbb;}
     .thumbnail {min-height: 210px}
+    .green {color: #003300 !important;}
+    .bold {font-weight: bold;}
   `]
 })
 
 
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    // THIS CAN BE DONE IN MULTIPLE WAYS
+    // Method #1
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return {green: isEarlyStart, bold: isEarlyStart};
+
+    // Method #2 - return String
+    // if (this.event && this.event.time === '8:00 am') {
+      // return 'green bold';
+    // }
+    // return '';
+
+    // Method #3 - Return array
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
+  }
 }
